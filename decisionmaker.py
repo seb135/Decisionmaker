e8a47f0316e4cfea58b2e2ac077006120c99f309
seb135 <sebastiaan_honders@hotmail.com>
2023-03-11 13:25:36 +0100

    Update decisionmaker.py

--- decisionmaker.py
+++ decisionmaker.py
@@ -1,9 +1,14 @@
 from random import choice
+import pandas as pd
+
+options = ['Go for a walk', 'Play a video game', 'Do something productive', 'Netflix & Chill', 'Go shopping']
 
 #How are you feeling?
 print("What mood are you in?")
 
 mood = input()
 
-print(mood)
+outcome = choice(options)
+
+print("Since you are: " + mood + ", lets go for " + outcome)
 
