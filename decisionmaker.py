710d6f382d2305de44d180e04f38397848b6fe93
seb135 <sebastiaan_honders@hotmail.com>
2023-03-11 14:38:27 +0100

    Added GUI
    Added GUI CustomTkinter

--- decisionmaker.py
+++ decisionmaker.py
@@ -1,14 +1,37 @@
+import customtkinter as tk
 from random import choice
+
+tk.set_appearance_mode("dark")
+
+tk.set_default_color_theme("dark-blue")
 
 options = ['Go for a walk', 'Play a video game', 'Do something productive', 'Netflix & Chill', 'Go shopping']
 
-#How are you feeling?
-print("What mood are you in?")
+option = choice(options)
 
-mood = input()
+def show_response():
+    response = option
+    textbox.insert(tk.END, response)
 
-outcome = choice(options)
+root = tk.CTk()
+root.geometry("500x350")
 
-print("Since you are: " + mood + ", lets go for " + outcome)
+frame = tk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = tk.CTkLabel(master=frame, text='Login System')
+label.pack(pady=12, padx=10)
+
+entry1 = tk.CTkEntry(master=frame, placeholder_text="How do you feel?")
+entry1.pack(pady=12,padx=10)
+
+button = tk.CTkButton(master=frame, text='login', command=show_response)
+button.pack(pady=12,padx=10)
+
+textbox = tk.CTkTextbox(master=frame, height=12, width=300 )
+textbox.pack(pady=12,padx=10)
+
+root.mainloop()
+
 
 
